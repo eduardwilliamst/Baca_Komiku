49b18a91070938120c57c0d6beed73049d198802
eduardwilliam <willitairas@gmail.com>
2023-10-11 13:29:45 +0700

    Chapter 3

---
+++ app/src/main/java/com/example/bacakomiku/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.bacakomiku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+
+class MainActivity : AppCompatActivity() {
+
+    private var btnActivity: AppCompatButton? = null
+    private var btnFragment: AppCompatButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnActivity = findViewById(R.id.button_to_activity)
+        btnFragment = findViewById(R.id.button_to_fragment)
+
+        btnActivity?.setOnClickListener{
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        btnFragment?.setOnClickListener{
+//            startActivity(Intent(this, FragmentActivity::class.java))
+            Toast.makeText(this, "Sorry, Will be Update Soon!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
